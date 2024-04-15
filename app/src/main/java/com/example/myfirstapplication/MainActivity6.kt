98b4c1e612125cd80548eb0a80c88740bf387a33
RishaShuvo <islam.farisha@gmail.com>
2024-04-15 12:26:09 +0000

    Project

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity6.kt
@@ -0,0 +1,16 @@
+package com.example.myfirstapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main6)
+        val retuning = findViewById<Button>(R.id.button2)
+        retuning.setOnClickListener { val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)}
+    }
+}
